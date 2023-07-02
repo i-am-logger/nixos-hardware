f173d705ee657c2da6b0e299405abe4b3fa7b511
SnIcK <ido.samuelson@gmail.com>
2023-07-02 01:47:14 -0500

    added 16IRX8H Lenovo Legion 7i Pro (2013)

---
+++ lenovo/legion/16irx8h/default.nix
@@ -0,0 +1,23 @@
+{ lib, ... }:
+
+{
+  imports = [
+    ../../../common/cpu/intel
+    ../../../common/gpu/nvidia/prime.nix
+    ../../../common/pc/laptop
+    ../../../common/pc/laptop/ssd
+    ../../../common/hidpi.nix
+  ];
+
+  # Specify bus id of Nvidia and Intel graphics.
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:00:02:0";
+    nvidiaBusId = "PCI:01:00:0";
+  };
+
+  # Cooling management
+  services.thermald.enable = lib.mkDefault true;
+
+  # √(2560² + 1600²) px / 16 in ≃ 189 dpi
+  services.xserver.dpi = 189;
+}
